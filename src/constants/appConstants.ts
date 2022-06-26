46e99aef01ef39a6c39d84218067d1ee68d7a13c
Kevin Nguyen <kevin.nguyen1996@gmail.com>
2022-06-26 20:20:38 +1000

    Complete

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,11 @@
+//It's good practice to have global constants all capitalised
+export const APP_NAME = 'Super app'
+export const WIDGETS_LABEL = 'Widgets'
+
+//should also define constant objects here
+export const apiStatus = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  SUCCESS: 'success',
+  ERROR: 'error',
+}
